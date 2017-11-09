a7f7db62544b97b0f4784d1867f5732ab07391cd
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-11-09 16:02:26 -0500

    clean up outout

--- SieveOfEratosthenes/Program.cs
+++ SieveOfEratosthenes/Program.cs
@@ -23,12 +23,18 @@ namespace SieveOfEratosthenes
             // You will compute 1,000,000 primes
             primes = sieve.Solve(1, 15485864); // The 1,000,000th prime is 15,485,863
             stopWatch.Stop();
+            int errorCount = 0;
             Console.Write(name + ": " + stopWatch.ElapsedMilliseconds.ToString() + " Milliseconds, ");
             try
             {
                 Console.WriteLine("returned " + primes.Count + " primes, first = " + primes[0] + " last = " + primes[primes.Count - 1]);
-            } catch (Exception ex) {Console.WriteLine("ERROR: " + ex.Message);
+                if (primes.Count != 1000000) { Console.Write("number of primes is INCORRECT"); errorCount++; }
+                if (primes[0] != 2) { Console.Write("first prime is INCORRECT"); errorCount++; }
+                if (primes[primes.Count - 1] != 15485863) { Console.Write("last prime is INCORRECT"); errorCount++; }
             }
+            catch (Exception ex) { Console.WriteLine("ERROR: " + ex.Message); errorCount++; }
+            if (errorCount == 0) { Console.WriteLine("all tests passed for " + name); }
+                            else { Console.WriteLine(errorCount + " test(s) failed for " + name + "."); }
         }
     }
 }
